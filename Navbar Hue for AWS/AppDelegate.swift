c07f73db95b71504b833f64e5c17da7abafdb5d7
dbektas <dino@bekta.ch>
2021-04-16 11:19:15 +0200

    Initial Commit

---
+++ Navbar Hue for AWS/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Navbar Hue for AWS
+//
+//  Created by Dino Bektas on 16.04.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
